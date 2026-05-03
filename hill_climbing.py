90a43b8583c9f482cf568b38070340c2b20e24c7
Parmida Beiki <hyperstellar11034@gmail.com>
2026-05-03 23:12:42 +0330

    Refactor hill climbing algorithm to track history

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,43 +1,36 @@
-from n_queens_base import count_conflicts, random
-import time
+from n_queens_base import count_conflicts, random, time
 
 def hill_climbing(n, max_restarts=100):
     start_time = time.time()
-    iterations = 0
+    history = []
+    total_iterations = 0
     
     for r in range(max_restarts):
-        # Initial random state
         current_state = [random.randint(0, n - 1) for _ in range(n)]
         current_conflicts = count_conflicts(current_state)
         
         while True:
-            iterations += 1
-            neighbor_found = False
-            best_neighbor = list(current_state)
-            min_conflicts = current_conflicts
+            total_iterations += 1
+            history.append(current_conflicts)
             
-            # Check all possible neighbors
+            if current_conflicts == 0:
+                return current_state, 0, total_iterations, time.time() - start_time, history
+            
+            neighbors = []
             for i in range(n):
                 for j in range(n):
-                    if current_state[i] == j:
-                        continue
-                    
-                    temp_state = list(current_state)
-                    temp_state[i] = j
-                    conflicts = count_conflicts(temp_state)
-                    
-                    if conflicts < min_conflicts:
-                        min_conflicts = conflicts
-                        best_neighbor = temp_state
-                        neighbor_found = True
+                    if current_state[i] != j:
+                        temp = list(current_state)
+                        temp[i] = j
+                        neighbors.append((temp, count_conflicts(temp)))
             
-            if not neighbor_found:
-                break # Stuck in local optimum
+            # Select the best neighbor
+            best_neighbor, min_conflicts = min(neighbors, key=lambda x: x[1])
+            
+            if min_conflicts >= current_conflicts:
+                break # Local optimum reached, will restart
             
             current_state = best_neighbor
             current_conflicts = min_conflicts
             
-            if current_conflicts == 0:
-                return current_state, current_conflicts, iterations, time.time() - start_time
-                
-    return current_state, current_conflicts, iterations, time.time() - start_time
+    return current_state, current_conflicts, total_iterations, time.time() - start_time, history
